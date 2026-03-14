adfaecc42dfea56398ef6fe65d79006644f8c030
arun79cg-cloud <arun79cg@gmail.com>
2026-03-14 15:12:46 +0530

    Update vite.config.js
    proxy added

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,18 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api/tickers': {
+        target: 'https://api.coindcx.com',
+        changeOrigin: true,
+        rewrite: (path) => '/exchange/ticker',
+      },
+      '/api/markets': {
+        target: 'https://api.coindcx.com',
+        changeOrigin: true,
+        rewrite: (path) => '/exchange/v1/markets_details',
+      },
+    },
+  },
 });
